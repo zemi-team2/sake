862e6b8b7849476c2ffa122268b35e7b8c4d5946
Wataru Ono <wtr0414@gmail.com>
2021-08-27 21:35:55 +0900

    feat: flake8 のルールに従うため

--- src/test.py
+++ src/test.py
@@ -9,8 +9,8 @@ from tensorflow.keras.utils import to_categorical
 
 x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+x_train = x_train.astype("float32")
+x_test = x_test.astype("float32")
 x_train /= 255
 x_test /= 255
 y_train = to_categorical(y_train, 10)
@@ -18,14 +18,23 @@ y_test = to_categorical(y_test, 10)
 
 model = Sequential()
 model.add(InputLayer(input_shape=(784,)))
-model.add(Dense(10, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.add(Dense(10, activation="softmax"))
+model.compile(
+    loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+)
 
 epochs = 20
 batch_size = 128
-history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
+history = model.fit(
+    x_train,
+    y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=1,
+    validation_data=(x_test, y_test),
+)
 
 score = model.evaluate(x_test, y_test, verbose=1)
 print()
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
