d1df6c78f561bd7598fce71a74d26cefbe22dd21
mitsukawa <w074ff@yamaguchi-u.ac.jp>
2021-08-28 13:04:52 +0900

    [fix]: PRレビューのnitsを修正

--- src/js/control_camera.js
+++ src/js/control_camera.js
@@ -1,11 +1,11 @@
 /**
  * クライアントのカメラで撮影する関数
  * shutter_buttonを押すと撮影し、up_load_buttonで決定
- * @param  {String} videoId            カメラが映し出す映像を表示する<video>タグのid
- * @param  {String} canvasId           撮影した結果を表示する<canvas>タグのid
+ * @param  {String} videoId           カメラが映し出す映像を表示する<video>タグのid
+ * @param  {String} canvasId          撮影した結果を表示する<canvas>タグのid
  * @param  {String} shutterButtonId   撮影を開始するボタンのid
  * @param  {String} upLoadButton      撮影した画像を決定するボタンのid
- * @return {String}                     base64形式の画像（これは、2021年8月28日時点では変更の可能性あり）
+ * @return {String}                   base64形式の画像（これは、2021年8月28日時点では変更の可能性あり）
  */
 function controlCamera(videoId, canvasId, shutterButtonId, upLoadButton) {
     window.onload = () => {
@@ -38,7 +38,7 @@ function controlCamera(videoId, canvasId, shutterButtonId, upLoadButton) {
             });
 
         /**
-         *  撮影ボタン(ボタンが押された時の<video>の1フレームを<convas>に表示)
+         *  撮影ボタン(ボタンが押された時の<video>の1フレームを<canvas>に表示)
          */
         document.querySelector("#" + shutterButtonId).addEventListener("click", () => {
             video.style.display = '' // <video>タグを表示
@@ -51,7 +51,7 @@ function controlCamera(videoId, canvasId, shutterButtonId, upLoadButton) {
         });
 
         /**
-         *  決定ボタン(ボタンが押された時convasの画像をbase64に変換して返す)
+         *  決定ボタン(ボタンが押された時<canvas>の画像をbase64に変換して返す)
          */
         document.querySelector("#" + upLoadButton).addEventListener("click", () => {
             const imageBase64 = canvas.toDataURL('image/png');
