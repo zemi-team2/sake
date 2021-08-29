535bde63175307e7d9afd884cc0a20b562d91cc7
Wataru Ono <wtr0414@gmail.com>
2021-08-29 12:06:47 +0900

    build: バックエンドの CI がパスするか確認するため

--- backend/src/sample.py
+++ backend/src/sample.py
@@ -7,5 +7,5 @@ load_url = "https://zuma-lab.com"
 data = requests.get(load_url)
 html = BeautifulSoup(data.content, "html.parser")
 
-for element in html.find_all("h4"):
+for element in html.find_all("h1"):
     print(element.text)
