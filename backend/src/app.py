ca324d00bb7876e940a5f26b9f21138258383691
mitsukawa <w074ff@gmail.com>
2021-09-15 16:46:24 +0900

    fix: formタグでpostできるようにするため（バックエンド）

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, after_this_request, render_template
+from flask import Flask, request, jsonify, after_this_request, render_template, url_for
 import base64
 from PIL import Image
 from io import BytesIO
@@ -6,35 +6,37 @@ import numpy as np
 
 # Flaskの設定
 template_dir = "/sake/frontend/src/html"
-static_dir = "/sake/frontend/src/js"
+static_dir = "/sake/frontend/src"
 app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
+IMAGE = None  # 撮影した画像を格納する変数
 
-# ルートにリクエスト(/)があった時の処理 ※Reactで実装する場合は必要ない
+# トップページ
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# 画像処理のリクエスト(/imageProcessing)があった時の処理
-@app.route("/imageProcessing", methods=["POST"])
-def image_detection():
-    @after_this_request
-    def add_header(response):
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        return response
+# プレビュー画面（取得した画像を変数に格納）
+# 撮影した画像を変数に格納してプレビュー画面を表示
+@app.route("/previewImage", methods=["POST"])
+def image_preview():
 
+    global IMAGE
     # 送信された画像をデコードしてnumpyに変換
-    enc_data = request.form["img"]
-    dec_data = base64.b64decode(enc_data.split(",")[1])
-    img_pil = Image.open(BytesIO(dec_data))
-    img_np = np.array(img_pil)
+    enc_img = request.form["image"]
+    IMAGE = base64.b64decode(enc_img.split(",")[1])
 
-    # これ以降に画像処理を書いていく
-    print(img_np)
-    # img_pil.save("image.png", quality=95)
+    return render_template("preview.html", shotImage=enc_img)
 
-    return jsonify(out1=1, out2="text")
+
+# 撮影した画像を基に画像処理を行って結果画面を表示
+@app.route("/result")
+def result():
+    img_pil = Image.open(BytesIO(IMAGE))
+    image_np = np.array(img_pil)
+    print(image_np)
+    return "result"
 
 
 if __name__ == "__main__":
