8e3912eabbd56b6367655f11b0431ff281b1f69c
mitsukawa <w074ff@gmail.com>
2021-09-15 16:45:22 +0900

    fix: formタグでpostできるように修正

--- frontend/src/js/controlCamera.js
+++ frontend/src/js/controlCamera.js
@@ -4,12 +4,15 @@
  * @param  {String} videoId           カメラが映し出す映像を表示する<video>タグのid
  * @param  {String} canvasId          撮影した結果を表示する<canvas>タグのid
  * @param  {String} shutterButtonId   撮影を開始するボタンのid
+ * @param  {String} formImageId       撮影した画像を送信するform要素
  * @return {void}
  */
-function controlCamera(videoId, canvasId, shutterButtonId) {
+
+function controlCamera(videoId, canvasId, shutterButtonId, formImageId) {
   window.onload = () => {
     const video = document.getElementById(videoId);
     const canvas = document.getElementById(canvasId);
+    const formImage = document.getElementById(formImageId);
 
     // カメラの設定
     const constraints = {
@@ -39,53 +42,13 @@ function controlCamera(videoId, canvasId, shutterButtonId) {
     document
       .getElementById(shutterButtonId)
       .addEventListener("click", () => {
-        video.style.display = ""; // <video>タグを表示
         const canvasContext = canvas.getContext("2d");
         const w = video.offsetWidth;
         const h = video.offsetHeight;
         canvas.setAttribute("width", w);
         canvas.setAttribute("height", h);
         canvasContext.drawImage(video, 0, 0, w, h);
+        formImage.value = canvas.toDataURL("image/png");
       });
   };
-}
-
-/**
- * カメラで撮影した画像をサーバーに送信する関数
- * uploadボタンを押すとサーバー画像を送信する
- * @param  {String} videoId           カメラが映し出す映像を表示する<video>タグのid
- * @param  {String} canvasId          撮影した結果を表示する<canvas>タグのid
- * @param  {String} uploadButtonId    撮影した画像を送信するボタンのid
- * @return {void}
- */
-
-function uploadImageToServer(videoId, canvasId, uploadButtonId) {
-  // 送信ボタンが押されたときのイベント処理（<canvasの画像を送信できる形式に変換して、画像処理サーバーに送信>）
-  const uploadButton = document.getElementById(uploadButtonId)
-  const video = document.getElementById(videoId);
-  const canvas = document.getElementById(canvasId);
-
-  uploadButton.addEventListener("click", () => {
-    // 画像をbase64に変換
-    const video = document.querySelector(`#${videoId}`);
-    const canvas = document.querySelector(`#${canvasId}`);
-    const imageBase64 = canvas.toDataURL("image/png");
-    video.style.display = "none"; // <video>タグを非表示
-    const formData = new FormData();
-    formData.append("img", imageBase64);
-    // console.log(`image: ${imageBase64}`);
-
-    // 画像をPOST方式で送信 url：http://127.0.0.1:8000/imageProcessing
-    axios
-      .post("http://127.0.0.1:8000/imageProcessing", formData)
-      .then((res) => {
-        // 通信成功時の処理
-        console.log(`Success ${res.data.out1}`);
-        console.log(`Success ${res.data.out2}`);
-      })
-      .catch((err) => {
-        // 通信失敗時の処理
-        console.error(`Error: ${err}`);
-      });
-  });
 }
