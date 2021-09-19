1f89df54bb59dc674caae24837900ce8324b4b10
mitsukawa <w074ff@yamaguchi-u.ac.jp>
2021-09-19 11:51:13 +0900

    fix: 画像のフォーマットをjpegに変更するため

--- frontend/src/js/controlCamera.js
+++ frontend/src/js/controlCamera.js
@@ -41,13 +41,13 @@ function controlCamera(videoId, canvasId, shutterButtonId, formImageId) {
     // 撮影ボタンが押されたときのイベント処理（<video>の1フレームを<canvas>に表示）
     document.getElementById(shutterButtonId).addEventListener("click", () => {
       const canvasContext = canvas.getContext("2d");
-      const w = video.offsetWidth / 10;
-      const h = video.offsetHeight / 10;
+      const w = video.offsetWidth;
+      const h = video.offsetHeight;
 
       canvas.setAttribute("width", w.toString());
       canvas.setAttribute("height", h.toString());
       canvasContext.drawImage(video, 0, 0, w, h);
-      formImage.value = canvas.toDataURL("image/png");
+      formImage.value = canvas.toDataURL("image/jpeg", 0.75); // 0.75だと上手くいっ
     });
   };
 }
